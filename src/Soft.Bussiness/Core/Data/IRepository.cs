18b69406cb3b7ac361753dac757fbfbaa5919e35
frederico101 <fredyalves.fredyalves@hotmail.com>
2024-08-10 17:30:20 -0300

    Implementa o repositorio generico

---
+++ src/Soft.Bussiness/Core/Data/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using Soft.Bussiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soft.Bussiness
+{
+    public interface IRepository<Tentity>: IDisposable where Tentity: Entity  
+    {
+        Task Add(Tentity entity);
+        Task<Tentity> GetById(Guid id);
+        Task<List<Tentity>> GetAll();
+        Task Update(Tentity entity);
+        Task Delete(Guid id);
+
+        Task<int> SaveChanges();
+    }
+}
